fcece85d3838367ed4c55a6941b2807677e01682
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2016-07-15 14:08:53 +0200

    added for testing

--- 03.KIE-communityRelease_copy-to-Nexus.sh
+++ 03.KIE-communityRelease_copy-to-Nexus.sh
@@ -24,4 +24,4 @@ echo TARGET=community >> kie.properties
 # cd $DEPLOY_DIR
 
 # (2) upload the content to remote staging repo
-mvn -B -e org.sonatype.plugins:nexus-staging-maven-plugin:1.6.5:deploy-staged-repository -DnexusUrl=https://repository.jboss.org/nexus -DserverId=jboss-releases-repository -DrepositoryDirectory=$DEPLOY_DIR -DstagingProfileId=15c58a1abc895b -DstagingDescription="kie-$KIE_VERSION" -DstagingProgressTimeoutMinutes=30
+mvn -B -e org.sonatype.plugins:nexus-staging-maven-plugin:1.6.5:deploy-staged-repository -DnexusUrl=https://repository.jboss.org/nexus -DserverId=jboss-releases-repository -DrepositoryDirectory=$DEPLOY_DIR -DstagingProfileId=15c58a1abc895b -DstagingDescription="kie-$KIE_VERSION-test" -DstagingProgressTimeoutMinutes=30
