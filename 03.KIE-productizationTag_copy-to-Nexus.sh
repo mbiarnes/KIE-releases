8886d2c7657ad9fe82525f202ca9dba39633d4ba
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2016-07-15 14:45:18 +0200

    added scripy for copy to Nexus productized

--- 03.KIE-productizationTag_copy-to-Nexus.sh
+++ 03.KIE-productizationTag_copy-to-Nexus.sh
@@ -9,18 +9,15 @@ fi
 cd $HOME/.jenkins/workspace/02.build-deploy-6.4.x/droolsjbpm-build-bootstrap
 KIE_VERSION=$(sed -e 's/^[ \t]*//' -e 's/[ \t]*$//' -n -e 's/<version.org.kie>\(.*\)<\/version.org.kie>/\1/p' pom.xml) 
 
-# copy Deploy_dir
-cd $WORKSPACE
-cp -r $HOME/.jenkins/workspace/02.build-deploy-6.4.x/Deploy_dir .
-
 cd $WORKSPACE
 
 #creates a properties file to pass variables
 echo KIE_VERSION=$KIE_VERSION > kie.properties
-echo TARGET=community >> kie.properties
+echo TARGET=productized >> kie.properties
 
-DEPLOY_DIR=Deploy-dir
+DEPLOY_DIR=Deploy_dir
+
 cd $HOME/.jenkins/workspace/02.build-deploy-6.4.x
 
 # upload the content to remote staging repo
-mvn -B -e org.sonatype.plugins:nexus-staging-maven-plugin:1.6.5:deploy-staged-repository -DnexusUrl=https://repository.jboss.org/nexus -DserverId=jboss-releases-repository -DrepositoryDirectory=$DEPLOY_DIR -DstagingProfileId=15c58a1abc895b -DstagingDescription="kie-$KIE_VERSION-test" -DstagingProgressTimeoutMinutes=30
+mvn -B -e org.sonatype.plugins:nexus-staging-maven-plugin:1.6.5:deploy-staged-repository -DnexusUrl=https://repository.jboss.org/nexus -DserverId=jboss-releases-repository -DrepositoryDirectory=$DEPLOY_DIR -DstagingProfileId=15c3321d12936e -DstagingDescription="kie-$KIE_VERSION-test-productized" -DstagingProgressTimeoutMinutes=30
