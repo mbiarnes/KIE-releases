34d62f08bcf42edc15de9c38cf87589d8abebad5
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2016-07-19 19:22:12 +0200

    test

--- 03a.KIE-releases_test-coverage-matrix.sh
+++ 03a.KIE-releases_test-coverage-matrix.sh
@@ -1,4 +1,4 @@
-cp $HOME/.jenkins/workspace/03.copy-to-Nexus-6.4.x/kie.properties .
+# cp $HOME/.jenkins/workspace/03.copy-to-Nexus-6.4.x/kie.properties .
 
 KIE_VERSION=$(sed -n -e '/KIE_VERSION/ s/.*\= *//p' kie.properties)
 TARGET=$(sed -n -e '/TARGET/ s/.*\= *//p' kie.properties)
@@ -11,7 +11,7 @@ fi
 
 echo "KIE version: $KIE_VERSION"
 echo "TARGET : $TARGET"
-echo "TRIGGER THE JOB WORKS"
+#echo "TRIGGER THE JOB WORKS"
 
 # wget the tar.gz sources
 wget -q https://repository.jboss.org/nexus/content/groups/$STAGING_REP/org/jbpm/jbpm/$KIE_VERSION/jbpm-$KIE_VERSION-project-sources.tar.gz -O sources.tar.gz
