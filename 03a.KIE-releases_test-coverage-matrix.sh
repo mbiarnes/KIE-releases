14788cc07e6d22c41855b0334617455637da8bc7
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2016-07-19 17:29:27 +0200

    fixed path

--- 03a.KIE-releases_test-coverage-matrix.sh
+++ 03a.KIE-releases_test-coverage-matrix.sh
@@ -1,4 +1,4 @@
-cp $HOME/.jenkins/03.copy-to-Nexus-6.4.x/kie.properties .
+cp $HOME/.jenkins/workspace/03.copy-to-Nexus-6.4.x/kie.properties .
 
 KIE_VERSION=$(sed -n -e '/KIE_VERSION/ s/.*\= *//p' kie.properties)
 TARGET=$(sed -n -e '/TARGET/ s/.*\= *//p' kie.properties)
