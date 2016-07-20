2d5ba20b25aeb2c3f8bbe25eeeae5588fa7a9703
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2016-07-20 09:27:30 +0200

    disabled temporarily KIE_VERSION

--- 03a.KIE-releases_test-coverage-matrix.sh
+++ 03a.KIE-releases_test-coverage-matrix.sh
@@ -1,7 +1,9 @@
-# cp $HOME/.jenkins/workspace/03.copy-to-Nexus-6.4.x/kie.properties .
+# temporary disabled
+#KIE_VERSION=$(sed -n -e '/KIE_VERSION/ s/.*\= *//p' kie.properties)
+#TARGET=$(sed -n -e '/TARGET/ s/.*\= *//p' kie.properties)
 
-KIE_VERSION=$(sed -n -e '/KIE_VERSION/ s/.*\= *//p' kie.properties)
-TARGET=$(sed -n -e '/TARGET/ s/.*\= *//p' kie.properties)
+KIE_VERSION=6.4.1.20160720-042100
+TARGET=productized
 
 if [ "$TARGET" == "community" ]; then 
    STAGING_REP=kie-group
@@ -11,7 +13,6 @@ fi
 
 echo "KIE version: $KIE_VERSION"
 echo "TARGET : $TARGET"
-#echo "TRIGGER THE JOB WORKS"
 
 # wget the tar.gz sources
 wget -q https://repository.jboss.org/nexus/content/groups/$STAGING_REP/org/jbpm/jbpm/$KIE_VERSION/jbpm-$KIE_VERSION-project-sources.tar.gz -O sources.tar.gz
